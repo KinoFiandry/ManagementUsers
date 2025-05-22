b647ee49c45e405528e9ede7e75629aa13e1c003
KinoFiandry <rakotoarisoafiandryamboara@gmail.com>
2025-05-22 17:04:01 +0400

    Création des contrôleurs et pages, v0.5

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,16 +6,17 @@ use App\Entity\User;
 use App\Form\UserForm;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
-final class UserController extends AbstractController
+class UserController extends AbstractController
 {
-    #[Route(name: 'app_user_index', methods: ['GET'])]
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -24,26 +25,32 @@ final class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-    {
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
         $user = new User();
-        $form = $this->createForm(UserForm::class, $user);
+        $form = $this->createForm(UserForm::class, $user, [
+            'is_new' => true, // Pour afficher les champs de mot de passe
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-        // Hacher le mot de passe avant l'enregistrement
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $user->getMotdepasse()
-        );
-        $user->setMotdepasse($hashedPassword);
-        
-        $entityManager->persist($user);
-        $entityManager->flush();
-        $this->addFlash('success', 'Utilisateur créé avec succès');
+            // Hachage du mot de passe
+            $plainPassword = $form->get('motdepasse')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setMotdepasse($hashedPassword);
+            }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a été créé avec succès.');
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
 
         return $this->render('user/new.html.twig', [
             'user' => $user,
@@ -60,37 +67,70 @@ final class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $form = $this->createForm(UserForm::class, $user);
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $form = $this->createForm(UserForm::class, $user, [
+            'is_new' => false, // Pour gérer différemment le mot de passe en édition
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-        //
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $user->getMotdepasse()
-        );
-        $user->setMotdepasse($hashedPassword);
-        $entityManager->flush();
+            // Gestion du mot de passe (seulement si fourni)
+            $plainPassword = $form->get('motdepasse')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setMotdepasse($hashedPassword);
+            }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-    }
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a été mis à jour avec succès.');
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
+            'edit_mode' => true,
         ]);
     }
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+    public function delete(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager
+    ): Response {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
+            
+            $this->addFlash('success', 'L\'utilisateur a été supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide, impossible de supprimer l\'utilisateur.');
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/search', name: 'app_user_search', methods: ['GET'])]
+    public function search(Request $request, UserRepository $userRepository): Response
+    {
+        $query = $request->query->get('q');
+        $users = [];
+
+        if ($query) {
+            $users = $userRepository->searchByQuery($query);
+        }
+
+        return $this->render('user/_search_results.html.twig', [
+            'users' => $users,
+            'query' => $query,
+        ]);
     }
 }
--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -6,9 +6,10 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserForm extends AbstractType
 {
@@ -17,19 +18,56 @@ class UserForm extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'label' => 'Nom',
-                'attr' => ['class' => 'form-control']
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez le nom'
+                ],
+                'row_attr' => [
+                    'class' => 'mb-3'
+                ]
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Prénom',
-                'attr' => ['class' => 'form-control']
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez le prénom'
+                ],
+                'row_attr' => [
+                    'class' => 'mb-3'
+                ]
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
-                'attr' => ['class' => 'form-control']
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez l\'email'
+                ],
+                'row_attr' => [
+                    'class' => 'mb-3'
+                ]
             ])
-            ->add('motdepasse', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'attr' => ['class' => 'form-control']
+            ->add('motdepasse', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'options' => [
+                    'attr' => ['class' => 'form-control password-field'],
+                    'row_attr' => ['class' => 'mb-3']
+                ],
+                'required' => $options['is_new'],
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Entrez le mot de passe',
+                        'autocomplete' => 'new-password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez le mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez le mot de passe',
+                        'autocomplete' => 'new-password'
+                    ]
+                ],
             ])
         ;
     }
@@ -38,6 +76,9 @@ class UserForm extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'is_new' => true,
         ]);
+        
+        $resolver->setAllowedTypes('is_new', 'bool');
     }
 }
